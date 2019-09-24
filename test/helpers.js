3fb939da7559f5563fc489a3781b8319d3747685
Tony Brix <tony@brix.ninja>
2019-09-24 11:58:19 -0500

    add resetDatabase

---
+++ test/helpers.js
@@ -0,0 +1,11 @@
+module.exports = {
+	async resetDatabase(sequelize, dialect, database) {
+		if (dialect === "mssql") {
+			await sequelize.query(`ALTER DATABASE ${database} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;`);
+		}
+
+		await sequelize.query(`DROP DATABASE ${database};`);
+		await sequelize.query(`CREATE DATABASE ${database};`);
+		await sequelize.query(`USE ${database};`);
+	},
+};
