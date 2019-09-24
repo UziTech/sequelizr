09656cafdd7ba1276d3672722a6d64266ab8c7af
Tony Brix <tony@brix.ninja>
2019-09-24 13:10:44 -0500

    log dialect

--- test/integration.test.js
+++ test/integration.test.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const files = fs.readdirSync(path.resolve(__dirname, "./integration"));
 
+console.log("DIALECT:", process.env.DIALECT);
+
 describe("integration", () => {
 	files.forEach(file => {
 		describe(file, require(path.resolve(__dirname, "./integration", file)));
