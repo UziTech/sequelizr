3501456e62253b750f2e175e6eeaa29f5dbcafd8
gdbonino <gdbonino@gmail.com>
2024-01-16 16:31:19 -0300

    CS

--- src/check-models.js
+++ src/check-models.js
@@ -99,18 +99,18 @@ async function checkModels(options = {}) {
 					dbColumns.push("updatedAt");
 					const columns = Object.keys(model.rawAttributes);
 					const realNameColumns = columns.map(col => {
-                       return 'field' in model.rawAttributes[col] ? model.rawAttributes[col].field : col; 
-                    });
+						return 'field' in model.rawAttributes[col] ? model.rawAttributes[col].field : col; 
+					});
                   
 					realNameColumns.forEach(col => {
-                       const originalColumnName = columns[realNameColumns.indexOf(col)];
-                       const type = convertToGenericType(model.rawAttributes[originalColumnName].type.toString());             
-                       if(type !== 'VIRTUAL') {
-    						if (!dbColumns.includes(col)) {
-    							logError(`'${table}.${col}' not in db`);
-    							isError = true;
-    						}                         
-                       }
+						const originalColumnName = columns[realNameColumns.indexOf(col)];
+						const type = convertToGenericType(model.rawAttributes[originalColumnName].type.toString());             
+						if(type !== 'VIRTUAL') {
+							if (!dbColumns.includes(col)) {
+								logError(`'${table}.${col}' not in db`);
+								isError = true;
+							}                         
+						}
 					});
 
 					for (const col in dbModel) {
@@ -118,7 +118,7 @@ async function checkModels(options = {}) {
 							logError(`'${table}.${col}' not in model`);
 							isError = true;
 						} else {
-                           const originalColumnName = columns[realNameColumns.indexOf(col)]
+							const originalColumnName = columns[realNameColumns.indexOf(col)]
 							const type = convertToGenericType(model.rawAttributes[originalColumnName].type.toString());
 							const dbType = convertToGenericType(dbModel[col].type);
 							if (type !== dbType) {
@@ -136,10 +136,10 @@ async function checkModels(options = {}) {
 						logError(`'${table}' Error: ${ex.message}`);
 					}
 				}
-				// if (!isError) {
-				// 	isError = true;
-				// 	logError(`'${table}' text has changed`);
-				// }
+				if (!isError) {
+					isError = true;
+				 	logError(`'${table}' text has changed`);
+				}
 			}
 		} catch (ex) {
 			isError = true;
